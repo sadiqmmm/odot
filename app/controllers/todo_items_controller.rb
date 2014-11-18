28039a2311161775e1128b4d4e09bf8900badab2
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-11-18 23:50:08 +0530

    Add flash message to todoitem and fixed bug in new method

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,8 +6,23 @@ class TodoItemsController < ApplicationController
   def new
   	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
+    end
+  
+  def create
+  	@todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
+
+    if @todo_item.save
+  		flash[:success] = "Added Todo list Item."
+  		redirect_to todo_list_todo_items_path
+  	else
+  		flash[:error] = "There was a problem adding todo list item."
+  		render action: :new
+  	end
   end
 
-  def create
-  end  
+  private
+    def todo_item_params
+    	params.require(:todo_item).permit(:content)
+    end
 end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,6 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_lists
+
+  validates :content, presence: true, 
+                      length: { minimum: 2 }
 end
