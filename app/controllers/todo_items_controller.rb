4eea36ccf698a0009e0ea53d0ff31536e3ac1f00
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-11-18 23:24:33 +0530

    Fixed a Bug in Todo List

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,7 +5,7 @@ class TodoItemsController < ApplicationController
 
   def new
   	@todo_list = TodoList.find(params[:todo_list_id])
-  	@todo_item = @todo_list.todo_item.new
+  	@todo_item = @todo_list.todo_items.new
   end
 
   def create
